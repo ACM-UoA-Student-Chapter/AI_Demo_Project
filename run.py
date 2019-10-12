0c1b99cad7912ba829644ed31ddeb9cd92c732e6
Pooja Vinod <37141925+poojavinod100@users.noreply.github.com>
2019-10-12 11:39:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -59,8 +59,8 @@ class EncryptedDB():
                 print("Value 2 is not a string. Sum not possible")
         else:
            print("Value 1 is not a string. Sum not possible") 
-                 
-    def AVG(self,key_1,key_2):                                          #receiving key values as parameters
+        
+     def MIN(self,key_1,key_2):                                         #receiving key values as parameters
         value_1=db.query(key_1)                                         #query value of first key from encrypted DB
         value_2=db.query(key_2)                                         #query value of second key from encrypted DB
         value_1.replace(' ','')                                         #remove spaces from value_1 (ex: if number contains spaces like in "(123) 456")
@@ -71,13 +71,15 @@ class EncryptedDB():
         value_2.replace(')','')                                         #remove ')' from value_2 (ex: if number contains spaces like in "(123) 456" as in example given)
         if value_1.isdigit():                                           #isdigit() returns true if string contains all digits, returns false if one non-digit found. Here, checking if value 1 is a string of digits
             if value_2.isdigit():                                       #check if value_2 contains digits only
-                avg=(int(value_1)+int(value_2))/2.0                     #calculate average of the two queried values
-                print("Average="+avg)
+               if int(value_1)<=int(value_2):                           #check if value_1 < value_2
+                    print("Minimum value:"+value_1+"(key_1)")           #print value_1 as minimum value
+               else:
+                    print("Minimum value:"+value_2+"(key_2)")           #print value_2 as minimum value
             else:
-                print("Value 2 is not a string. Average operation not possible")
+                print("Value 2 is not a string. Min operation not possible")
         else:
-           print("Value 1 is not a string. Average operation not possible")          
-            
+           print("Value 1 is not a string. Min operation not possible") 
+                 
 bob = sy.VirtualWorker(hook, id="bob").add_worker(sy.local_worker)
 alice = sy.VirtualWorker(hook, id="alice").add_worker(sy.local_worker)
 secure_worker = sy.VirtualWorker(hook, id="secure_worker").add_worker(sy.local_worker)
